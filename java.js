84a2c9da6f2db3267d53a9c01804f77a643f0c34
03freeezee <156226012+03freeezee@users.noreply.github.com>
2025-04-20 18:11:12 +0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,27 @@
+function abouttarget() {
+    const targetElement = document.getElementById('about');
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+}
+
+function introductiontarget() {
+    const targetElement = document.getElementById('introduction');
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+}
+
+
+function skilltarget() {
+    const targetElement = document.getElementById('skills');
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+}
+
+
+function abouttarget() {
+    const targetElement = document.getElementById('about');
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+}
+
+function servicetarget() {
+    const targetElement = document.getElementById('service-locator');
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+}
+
